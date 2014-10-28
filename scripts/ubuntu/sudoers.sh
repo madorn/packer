179807d45f941624935ab5564226dac10239c96b
Matthew Dorn <madorn@gmail.com>
2014-10-28 17:47:32 -0500

    remove sudo

--- scripts/ubuntu/sudoers.sh
+++ scripts/ubuntu/sudoers.sh
@@ -2,4 +2,4 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=NOPASSWD:ALL/g' /etc/sudoers
-echo root:vagrant | sudo chpasswd
+echo root:vagrant | chpasswd
